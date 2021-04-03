3d755ed3633125a2c73b3bae38f8b8f87cc46fe7
刘致君01 <liuzhijun01@qianxin.com>
2021-04-03 17:17:11 +0800

    use re.compile

--- degree.py
+++ degree.py
@@ -1,82 +1,15 @@
 # coding: utf-8
-import re
+from pattern import *
 import csv
 import os
-
-
-class DegreePattern:
-    # 博士后
-    _POSTDOC_PATTERN = (
-        r"(postdoc)",
-    )
-    # 博士
-    _PHD_PATTERN = (
-        r"(doctor)", r"(phd)", r"(ph\.d)"
-    )
-    # 医学博士
-    _MD_PATTERN = (
-        r"(doctor of medicine)", r"(md)"
-    )
-    # 法律博士
-    _JD_PATTERN = (
-        r"(jd)", "(doctor of law)", "(juris)"
-    )
-    # 工商管理硕士
-    _MBA_PATTERN = (
-        r"(mba)", r"(master of business administration)"
-    )
-    # 硕士学位
-    _MASTER_PATTERN = (
-        r"(master)", r"(ms)",
-    )
-    # 学士学位
-    _BACHELOR_PATTERN = (
-        r"(bachelor)", r"(bs)", r"(ba)", r"(b\.s)", r"(b\.a)", r"(b\.e)", "(engineer)"
-    )
-    # 副学士学位
-    _ASSOCIATE_PATTERN = (
-        r"(associate)", r"(a\.a\.)"
-    )
-
-    patterns = (
-
-        _POSTDOC_PATTERN,
-
-        _JD_PATTERN,
-
-        _MD_PATTERN,
-
-        _PHD_PATTERN,
-
-        _MBA_PATTERN,
-
-        _MASTER_PATTERN,
-
-        _BACHELOR_PATTERN,
-
-        _ASSOCIATE_PATTERN,
-    )
-    regular_dict = {
-        _POSTDOC_PATTERN: "POSTDOC",
-        _PHD_PATTERN: "PHD",
-        _MD_PATTERN: "MD",
-        _JD_PATTERN: "JD",
-        _MBA_PATTERN: "MBA",
-        _MASTER_PATTERN: "MASTER",
-        _BACHELOR_PATTERN: "BACHELOR",
-        _ASSOCIATE_PATTERN: "ASSOCIATE",
-        "": "没有找到学位"
-    }
 
 
 class DegreeRegular(DegreePattern):
     def _regular(self, text):
         for pattern in self.patterns:
-            _pattern = "|".join(pattern)
-            res = re.search(_pattern, text, flags=re.IGNORECASE)
-            if res:
-                return self.regular_dict[pattern]
-        return self.regular_dict[""]
+            if pattern.regular_text(text):
+                return text
+        return UNKNOWN_REGULAR
 
     def regular_text(self, text):
         return self._regular(text)
---
+++ pattern.py
@@ -0,0 +1,118 @@
+import re
+
+
+UNKNOWN_REGULAR = "未找到学位"
+
+
+class Pattern:
+    pattern = ()
+    regular = ""
+
+    def __init__(self):
+        self.prog = re.compile("|".join(self.pattern), flags=re.IGNORECASE)
+
+    def regular_text(self, text):
+        if self.prog.search(text):
+            return self.regular
+
+
+# 博士后
+class PostdocPattern(Pattern):
+    pattern = (
+        r"(postdoc)",
+    )
+
+    def __init__(self):
+        super(PostdocPattern, self).__init__()
+
+
+# 博士
+class PhdPattern(Pattern):
+    pattern = (
+        r"(doctor)", r"(phd)", r"(ph\.d)"
+    )
+    regular = "PHD"
+
+    def __init__(self):
+        super(PhdPattern, self).__init__()
+
+
+# 医学博士
+class MdPattern(Pattern):
+    pattern = (
+        r"(doctor of medicine)", r"(md)"
+    )
+    regular = "MD"
+
+    def __init__(self):
+        super(MdPattern, self).__init__()
+
+
+# 法律博士
+class JdPattern(Pattern):
+    pattern = (
+        r"(jd)", "(doctor of law)", "(juris)"
+    )
+    regular = "JD"
+
+    def __init__(self):
+        super(JdPattern, self).__init__()
+
+
+
+# 工商管理硕士
+class MbaPattern(Pattern):
+    pattern = (
+        r"(mba)", r"(master of business administration)"
+    )
+    regular = "MBA"
+
+    def __init__(self):
+        super(MbaPattern, self).__init__()
+
+
+# 硕士学位
+class MasterPattern(Pattern):
+    pattern = (
+        r"(master)", r"(ms)",
+    )
+    regular = "MASTER"
+
+    def __init__(self):
+        super(MasterPattern, self).__init__()
+
+
+# 学士学位
+class BachelorPattern(Pattern):
+    pattern = (
+        r"(bachelor)", r"(bs)", r"(ba)", r"(b\.s)", r"(b\.a)", r"(b\.e)", "(engineer)"
+    )
+    regular = "BACHELOR"
+
+    def __init__(self):
+        super(BachelorPattern, self).__init__()
+
+
+# 副学士学位
+class AssociatePattern(Pattern):
+    pattern = (
+        r"(associate)", r"(a\.a\.)"
+    )
+    regular = "ASSOCIATE"
+
+    def __init__(self):
+        super(AssociatePattern, self).__init__()
+
+
+class DegreePattern:
+    def __init__(self):
+        self.patterns = (
+            PostdocPattern(),
+            PhdPattern(),
+            MdPattern(),
+            JdPattern(),
+            MbaPattern(),
+            MasterPattern(),
+            BachelorPattern(),
+            AssociatePattern()
+        )
